76344287ab0c3d74138e2e8cc0795c83e848e21b
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-06-02 15:13:49 +0700

    Wiring Up Search(Add filter functionnally)

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -21,10 +21,6 @@ describe('TodoAPI', () => {
         TodoAPI.setTodos(todos);
 
         var actualTodos = JSON.parse(localStorage.getItem('todos'));
-        // []
-        // []
-        // var a = [];
-        // var b = a;
         expect(actualTodos).toEqual(todos);
     });
     it('should not set invalid todos array', () => {
@@ -51,9 +47,50 @@ describe('TodoAPI', () => {
 
         expect(actualTodos).toEqual(todos);
     });
+});
 
+    describe('filterTodos', () => {
+        var todos = [{
+            id: 1,
+            text: 'Some text here',
+            completed: true
+        },
+        {
+            id: 2,
+            text: 'other text here',
+            completed: false
+        },
+        {
+            id: 3,
+            text: 'Some text here',
+            completed: true
+        }];
 
-})
+        it('should return all items if showCompleted is true', () => {
+            var filterTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filterTodos.length).toBe(3);
+        });
+
+        it('should return non-completed todos when showCompleted is false', () => {
+            var filterTodos = TodoAPI.filterTodos(todos, false, '');
+            expect(filterTodos.length).toBe(1);
+        });
+
+        it('should sort by completed status', () => {
+            var filterTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filterTodos[0].completed).toBe(false);
+        });
+
+        it('should filter todos by searchText', () => {
+            var filterTodos = TodoAPI.filterTodos(todos, true, 'some');
+            expect(filterTodos.length).toBe(2);
+        });
+
+        it('should return all todos if searchText is empty', () => {
+            var filterTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filterTodos.length).toBe(3);
+        });
+    });
 
 
 });
