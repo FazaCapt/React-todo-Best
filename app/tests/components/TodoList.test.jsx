ce84e291eb07ad8fbb29763022f3bcde94202b5f
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-06-04 22:02:17 +0700

    Testing Reduxed Components (Part 2)

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -1,12 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Provider} = require('react-redux');
 var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
 var $ = require('jquery');
 
 // var TodoList = require('TodoList');
+import {configure} from 'configureStore';
 import ConnectedTodoList, {TodoList} from 'TodoList'
-var Todo = require('Todo');
+import ConnectedTodo, {Todo} from 'Todo';
+// var Todo = require('Todo');
 
 describe('TodoList', () => {
     it('should exist', () => { 
@@ -16,14 +19,29 @@ describe('TodoList', () => {
     it('should render one Todo component for each todo item', () => { 
         var todos = [{
             id: 1,
-            text: 'Do Something'
+            text: 'Do Something',
+            completed: false,
+            completedAt: undefined,
+            createdAt: 500
         },
         {
             id: 2,
-            text: 'Check Email'
+            text: 'Check Email',
+            completed: false,
+            completedAt: undefined,
+            createdAt: 500
         }];
-        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
-        var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+        var store = configure({
+            todos
+        });
+        var provider = TestUtils.renderIntoDocument(
+            <Provider store = {store}>
+                <ConnectedTodoList/>
+            </Provider>
+        );
+        // var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+        var todoList = TestUtils.scryRenderedComponentsWithType(provider, ConnectedTodoList)[0];
+        var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, ConnectedTodo);
 
         expect(todosComponents.length).toBe(todos.length);
     });
